687998f9fc787be29923d70663ee7c8bf3653cf6
JemyungLEE <leejimac@leejs-iMac.local>
2019-10-10 16:28:55 +0900

    India microdata integrity check modules.

---
+++ India/DataAnalizer.jl
@@ -0,0 +1,25 @@
+# Developed date: 10.Oct. 2019
+# Last modified date: 10. Oct. 2019
+# Subject: India microdata analyzer
+# Description: proceed data analysis process for India household consumption microdata
+# Developer: Jemyung Lee
+# Affiliation: RIHN (Research Institute for Humanity and Nature)
+
+clearconsole()
+cd(Base.source_dir())
+include("IntegrityChecker.jl")
+using .IntegrityChecker
+ic = IntegrityChecker
+
+microdata = ["test_lv5.txt", 20, 23, 24]    # level 5
+#microdata = ["test_lv6.txt", 20, 23, 24]    # level 6
+#microdata = ["test_lv7.txt", 20, 21, 22]    # level 7 for 30 days and 365 days monetary
+#microdata = ["test_lv8.txt", 20, 21, 21]    # level 8 for only 30 dats monetary
+#microdata = ["test_lv9.txt", 20, 28, 30]    # level 9
+
+inputFile = Base.source_dir()*"/"*microdata[1]
+integrityFile = replace(inputFile, ".txt" => "_integrity.txt")
+
+ic.checkIntegrity(inputFile, microdata[2], microdata[3], microdata[4])
+
+ic.printIntegrity(integrityFile)
---
+++ India/IntegrityChecker.jl
@@ -0,0 +1,79 @@
+module IntegrityChecker
+
+# Developed date: 10. Oct. 2019
+# Last modified date: 10. Oct. 2019
+# Subject: India household consumption microdata checker
+# Description: check each sector's data integrity reading all the lines
+# Developer: Jemyung Lee
+# Affiliation: RIHN (Research Institute for Humanity and Nature)
+
+mutable struct sector       # category data
+    code::String             # index number of the category item(sector)
+    desc::String            # description of the sector
+
+    total::UInt32           # total data number of the sector
+    nQt::UInt32             # number lines that conatin quantity data
+    nVl::UInt32             # number lines that conatin value data
+
+    isQt::Bool              # whether the sector has any quantity data (Kg, litre, no., ot etc.)
+    isVl::Bool              # whether the sector has any monetary value (Pr. or other currency)
+    allQt::Bool             # whether all the data-lines of the sector have quantity data
+    allVl::Bool             # whether all the data-lines of the sector have value data
+
+    function sector(cod, des="", tot=0, nq=0, nv=0, iqt=false, ivl=false, aqt=false, avl=false)
+        new(cod, des, tot, nq, nv, iqt, ivl, aqt, avl)
+    end
+end
+
+sectors = Dict{String, sector}()
+
+function checkIntegrity(inputFile, idxCd, idxQt, idxVl) # idx: index of, Cd: code, Qt: quantity, Vl: value
+
+    global sectors
+
+    f = open(inputFile)
+
+    readline(f)
+    for l in eachline(f)
+        tmpArray = split(l, '\t')
+        code = tmpArray[idxCd]
+
+        if !haskey(sectors, code)
+            sectors[code] = sector(code) end
+        s = sectors[code]
+        s.total += 1
+
+        if length(tmpArray[idxQt]) > 0 && isinteger(parse(Float32, tmpArray[idxQt]))
+            s.nQt += 1
+        end
+        if length(tmpArray[idxVl]) > 0 && isinteger(parse(Float32, tmpArray[idxVl]))
+            s.nVl += 1
+        end
+    end
+
+    for code in keys(sectors)
+        s= sectors[code]
+        if s.nQt > 0; s.isQt = true end
+        if s.nVl > 0; s.isVl = true end
+        if s.nQt == s.total; s.allQt = true end
+        if s.nVl == s.total; s.allVl = true end
+    end
+    close(f)
+end
+
+function printIntegrity(outputFile = "")
+
+    if length(outputFile)>0; f = open(outputFile, "w")
+    else f = IOBuffer()
+    end
+
+    println(f, "Code\tTotal\tQuantities\tValues\tAll_Q\tAll_V")
+    for code in sort(collect(keys(sectors)))
+        s = sectors[code]
+        println(f, s.code, "\t", s.total, "\t", s.nQt, "\t", s.nVl, "\t", s.allQt, "\t", s.allVl)
+    end
+
+    close(f)
+end
+
+end
