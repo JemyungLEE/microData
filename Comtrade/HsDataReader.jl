f24277c2bd8e93ec064b8d1b4c23d9713e0255ab
JemyungLEE <leejimac@leejs-iMac.local>
2019-10-21 17:02:13 +0900

    UN contrade data reader (not completed)

---
+++ Comtrade/HsDataReader.jl
@@ -0,0 +1,57 @@
+module HsDataReader
+
+# Developed date: 21. Oct. 2019
+# Last modified date: 21. Oct. 2019
+# Subject: Harmonized System (HS) UN comtrade data reader
+# Description: read trade data of HS classification
+# Developer: Jemyung Lee
+# Affiliation: RIHN (Research Institute for Humanity and Nature)
+
+mutable struct trade
+    year::Int16
+    flow::String
+    reporter::String
+    partner::String
+    code::String        # commodity HS code
+    desc::String        # commodity description
+    value::Int64        # trade value
+    netw::Int64         # net weight (Kg)
+    unit::String        # unit
+    quant::Int64           # trade quantity
+
+    orig::String        # origin
+    dest::String        # destination
+
+    function trade(yr::Int16, fl::String, rp="", pt="", cd="", dc="", vl=0, nt=0, un="", qt=0)
+        if fl == "Import"
+            or = pt
+            dt = rp
+        elseif fl == "Export"
+            or = rp
+            dt = pt
+        end
+        new(yr, fl, rp, pt, cd, dc, vl, nt, un, qt, or, dt)
+    end
+end
+
+trades = Array{trade, 1}
+
+function readTradeData(inputFile)
+    f = open(inputFile)
+
+    global trades = []
+    s = ""
+
+    readline(f)
+    for l in eachline(f)
+        s = split(l, ',')
+        s = [strip(c, '"') for c in s]
+        s = [string(c) for c in s]
+
+        push!(trades,trade(parse(Int16,s[1]),s[2],s[3],s[4],s[5],s[6],parse(Int64,s[7]),parse(Int64,s[8]),s[9],parse(Int64,s[10])))
+    end
+
+    return trades
+end
+
+end
---
+++ Comtrade/TradeAnalayzer.jl
@@ -0,0 +1,22 @@
+# Developed date: 21. Oct. 2019
+# Last modified date: 21. Oct. 2019
+# Subject: Harmonized System (HS) UN comtrade data analyzer
+# Description: analysis UN comtrade trading data
+# Developer: Jemyung Lee
+# Affiliation: RIHN (Research Institute for Humanity and Nature)
+
+clearconsole()
+cd(Base.source_dir())
+
+include("HsDataReader.jl")
+using .HsDataReader
+hdr = HsDataReader
+
+inputData = "hs-2011b.csv"
+inputData = Base.source_dir()*"/data/"*inputData
+
+#hdr.readTradeData(inputData)
+
+
+using CSV
+CSV.read(inputData)
