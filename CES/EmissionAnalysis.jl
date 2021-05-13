ef4aa1e0df736554427b7ffc07fa2e30a50234d1
Jemyung <leej@chikyu.ac.jp>
2021-05-13 10:53:04 +0900

    Minor changes

--- CES/EmissionAnalysis.jl
+++ CES/EmissionAnalysis.jl
@@ -1,5 +1,5 @@
 # Developed date: 13. Apr. 2021
-# Last modified date: 5. May. 2021
+# Last modified date: 7. May. 2021
 # Subject: Estimate carbon footprint by household consumptions
 # Description: Calculate direct and indirect carbon emissions
 #              by linking household consumptions and global supply chain,
@@ -21,105 +21,113 @@ mdr = MicroDataReader
 cmb = ConcMatBuilder
 ee = EmissionEstimator
 
-year = 2018
-eoraYear = 2015     # eoraYear = year
+cesYear = 2018; exchYear = cesYear
+eoraYear = 2015
 nation = "Indonesia"
 natA3 = "IDN"
+natCurr = "IDR"
+fitEoraYear = true      # scaling micro-data's expenditure to fit the Eora target year
+readMembers = false     # read member data
 
 filePath = Base.source_dir() * "/data/" * natA3 * "/"
 indexFilePath = filePath * "index/"
 microDataPath = filePath * "microdata/"
 extractedPath = filePath * "extracted/"
 emissionPath = filePath * "emission/"
+concordancePath = filePath * "index/concordance/"
 
-curConv = true; erfile = indexFilePath * "CurrencyExchangeRates.txt"
+curConv = true; curr_target = "USD"; erfile = indexFilePath * "CurrencyExchangeRates.txt"
 pppConv = false; pppfile = filePath * "PPP_ConvertingRates.txt"
 
 DE_conv = filePath * "index/EmissionCovertingRate.txt"
 
-IE_mode = false      # indirect carbon emission estimation
+IE_mode = true      # indirect carbon emission estimation
 DE_mode = false      # direct carbon emission estimation
 
 # Qtable = "I_CHG_CO2"
 Qtable = "PRIMAP"
-scaleMode = true
+scaleMode = false
+sparseMode = true
+quantMode = true
 
 if scaleMode; scaleTag = "_Scaled" else scaleTag = "" end
 
-regInfoFile = extractedPath * nation * "_" * string(year) * "_RegionInfo.txt"
-cmmfile = extractedPath * nation * "_" * string(year) * "_Commodities.txt"
-hhsfile = extractedPath * nation * "_" * string(year) * "_Households.txt"
-mmsfile = extractedPath * nation * "_" * string(year) * "_Members.txt"
-itemfile = indexFilePath * nation * "_" * string(year) * "_Commodity_items.txt"
-exdfile = extractedPath * nation * "_" * string(year) * "_Expenditure.txt"
-exmfile = extractedPath * nation * "_" * string(year) * scaleTag * "_Expenditure_matrix.txt"
+regInfoFile = extractedPath * natA3 * "_" * string(cesYear) * "_RegionInfo.txt"
+cmmfile = extractedPath * natA3 * "_" * string(cesYear) * "_Commodities.txt"
+hhsfile = extractedPath * natA3 * "_" * string(cesYear) * "_Households.txt"
+mmsfile = extractedPath * natA3 * "_" * string(cesYear) * "_Members.txt"
+itemfile = indexFilePath * natA3 * "_" * string(cesYear) * "_Commodity_items.txt"
+expfile = extractedPath * natA3 * "_" * string(cesYear) * "_Expenditure_"*natCurr*".txt"
+exmfile = extractedPath * natA3 * "_" * string(cesYear) * scaleTag * "_Expenditure_matrix_"*natCurr*".txt"
 
 println("[Process]")
-# print(" Category codes reading: ")
-# mdr.readCategory(categoryFile, depth=catDepth, catFile=ctgfile, inclAbr=abrExpMode)
-# println("completed")
 
 print(" Micro-data reading: ")
-print(", regions"); mdr.readPrintedRegionData(year, nation, regInfoFile)
-print(", households"); mdr.readPrintedHouseholdData(year, nation, hhsfile)
-# print(", members"); mdr.readPrintedMemberData(year, nation, mmsfile)
-print(", sectors"); mdr.readPrintedSectorData(year, nation, cmmfile)
-print(", expenditures(", expfile, ")"); mdr.readPrintedExpenditureData(readPrintedHouseholdDataexpfile)
-if curConv; print(", exchange"); mdr.exchangeExpCurrency(year, nation, erfile, inverse=true) end
-if PPPConv; print(", ppp"); mdr.convertToPPP(pppfile); println("complete") end
-print(", build matrix"); mdr.buildExpenditureMatrix(year, nation, expfile)
-# print(", matrix"); mdr.readPrintedExpenditureMatrix(year, nation, exmfile)
+print(", regions"); mdr.readPrintedRegionData(cesYear, natA3, regInfoFile)
+print(", households"); mdr.readPrintedHouseholdData(cesYear, natA3, hhsfile)
+if readMembers; print(", members"); mdr.readPrintedMemberData(cesYear, natA3, mmsfile) end
+print(", sectors"); mdr.readPrintedSectorData(cesYear, natA3, cmmfile)
+print(", expenditures(", rsplit(expfile, '/', limit=2)[2], ")"); mdr.readPrintedExpenditureData(cesYear, natA3, expfile, quantity=quantMode)
+if fitEoraYear && eoraYear != nothing && eoraYear != cesYear; print(" Expenditure scaling: from $cesYear to $eoraYear")
+    exchYear = eoraYear
+    cpiSecFile = indexFilePath * "CPI/CPI_"*natA3*"_sectors.txt"
+    statFile = indexFilePath * "CPI/CPI_"*natA3*"_values.txt"
+    linkFile = indexFilePath * "CPI/CPI_"*natA3*"_link.txt"
+    print(", scaling"); mdr.scalingExpByCPI(cesYear, natA3, cpiSecFile, statFile, linkFile, eoraYear, period="year", region="district", revHH=true, revMat=false)
+    println(" ... completed")
+end
+if curConv; print(", exchange"); mdr.exchangeExpCurrency(cesYear, exchYear, natA3, natCurr, erfile, target_curr=curr_target) end
+if pppConv; print(", ppp"); mdr.convertToPPP(eoraYear, natA3, pppfile); println("complete") end
+print(", matrix"); mes = mdr.buildExpenditureMatrix(cesYear, natA3, period = 365, quantity = quantMode)
+# print(", matrix"); mdr.readPrintedExpenditureMatrix(cesYear, natA3, exmfile)
 println(" ... completed")
 
 if IE_mode
-    # Converting process of Eora final demand data to India micro-data format
-    concordanceFile = filePath * "/" * natA3 * "/index/"* natA3 *"_EORA_Conc_ver0.9.xlsx"
-    conMatFile = filePath * "/" * natA3 * "/index/concordance/ConcMat.txt"
-    conSumMatFile = filePath * "/" * natA3 * "/index/concordance/ConcSumMat.txt"
-    print(" Concordance matrix building:")
-    # print(" xlsx reading"); cmb.readXlsxData(concordanceFile, nation, weight=false)
-    nationFile = filePath * "/" * natA3 * "/index/concordance/Eora_nations.txt"
-    sectorFile = filePath * "/" * natA3 * "/index/concordance/Eora_sectors.txt"
-    concFile = filePath * "/" * natA3 * "/index/concordance/LinkedSectors_IE.txt"
-    print(" data reading"); cmb.readConcMatFile(nationFile, sectorFile, concFile, nation, weight=false)
-    # linkSecFile = filePath * "/" * natA3 * "/index/concordance/LinkedSectors_IE.txt"
-    # print(", linkage printing"); cmb.exportLinkedSectors(linkSecFile, natA3, mrio="Eora")
-    print(", matrix builing"); cmb.buildConMat()
-    print(", normalization"); cmn = cmb.normConMat()   # {a3, conMat}
+    conMatFile = concordancePath * "ConcMat.txt"
+    conSumMatFile = concordancePath * "ConcSumMat.txt"
+    nationFile = concordancePath * "Eora_nations.txt"
+    sectorFile = concordancePath * "Eora_sectors.txt"
+    concFile = concordancePath * "LinkedSectors_IE.txt"
 
-    cmb.printConMat(conMatFile, nation, norm = true, categ = true)
-    # cmb.printSumNat(conSumMatFile, nation, norm = true)
+    print(" Concordance matrix building:")
+    print(" data reading"); cmb.readIeConcMatFile(nationFile, sectorFile, concFile, weight=false)
+    print(", matrix builing"); cmb.buildIeConMat()
+    print(", normalization"); cmn = cmb.normConMat()   # {a3, conMat}
+    print(", print matrix"); cmb.printConMat(conMatFile, natA3, norm = true, categ = true)
+    # cmb.printSumNat(conSumMatFile, natA3, norm = true)
     println(" complete")
 
-    # Eora household's final-demand import sector data reading process
     print(" MRIO table reading:")
     path = "../Eora/data/" * string(eoraYear) * "/" * string(eoraYear)
     print(" index"); ee.readIndex("../Eora/data/index/revised/")
     print(", table"); ee.readIOTables(eoraYear, path*"_eora_t.csv", path*"_eora_v.csv", path*"_eora_y.csv", path*"_eora_q.csv")
-    print(", rearrange"); ee.rearrangeIndex(qmode=Qtable); ee.rearrangeTables(year, qmode=Qtable)
+    print(", rearrange"); ee.rearrangeIndex(qmode=Qtable); ee.rearrangeTables(eoraYear, qmode=Qtable)
     print(", Leontief matrix"); ee.calculateLeontief(eoraYear)
     println(" complete")
 end
-# if DE_mode
-#     print(" Direct emission converting indices reading:")
-#     ee.readEmissionRates(year, categoryFile, DE_conv)
-#     println(" complete")
-# end
-
-println(" Emission calculation: ")
-ieFile = emissionPath * string(year) * "_" * natA3 * "_hhs_"*scaleTag*"IE_"*Qtable*".txt"
-deFile = emissionPath * string(year) * "_" * natA3 * "_hhs_"*scaleTag*"DE.txt"
-print(" data"); ee.getDomesticData(year, mdr.expTable[year], mdr.hhsList[year])
-# if DE_mode; print(", DE")
-#     ee.calculateDirectEmission(year, n)
-#     ee.printDirectEmissions(year, deFile)
-# end
-if IE_mode
-    print(", concordance"); ee.buildWeightedConcMat(year, ee.abb[mdr.nationNames[n]], cmn, output = filePath*"index/concordance/"*scaleTag*"Eurostat_Eora_weighted_concordance_table.csv")
-    print(", IE"); ee.calculateIndirectEmission(year)
-    ee.printIndirectEmissions(year, emissionFile)
+if DE_mode
+    print(" Direct emission preparing:")
+    print(" data reading"); ee.readEmissionIntensity(cesYear, natA3, sectorFile, intensityFile)
+    print(", concordance matrix building"); cmb.buildDeConcMat(natA3, deCodeFile, concFile, norm = true, output = "")
+    println(" complete")
 end
 
-println("complete")
+println(" Emission calculation: ")
+if DE_mode || IE_mode
+    ieFile = emissionPath * string(cesYear) * "_" * natA3 * "_hhs_"*scaleTag*"IE_"*Qtable*".txt"
+    deFile = emissionPath * string(cesYear) * "_" * natA3 * "_hhs_"*scaleTag*"DE.txt"
+    print(" data"); ee.getDomesticData(cesYear, natA3, mdr.hh_list[cesYear][natA3], mdr.sc_list[cesYear][natA3], mdr.expMatrix[cesYear][natA3])
+end
+if DE_mode; print(", DE")
+    ee.calculateDirectEmission(cesYear, natA3, commodity = quantMode, sparseMat = sparseMode)
+    ee.printDirectEmissions(cesYear, natA3, outputFile)
+end
+if IE_mode
+    conWghMatFile = filePath*"index/concordance/"*scaleTag*"Eurostat_Eora_weighted_concordance_table.csv"
+    print(", concordance"); ee.buildWeightedConcMat(cesYear, eoraYear, natA3, cmn, output = conWghMatFile)
+    print(", estimate_IE"); ee.calculateIndirectEmission(cesYear, eoraYear, natA3, sparseMat = sparseMode, elapChk = 1)
+    print(", print_IE"); ee.printIndirectEmissions(cesYear, natA3, ieFile)
+end
+println(" ... complete")
 
-println(" ... all complete")
+println("[all complete]")
