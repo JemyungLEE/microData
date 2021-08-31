f40790dfb2d1078e22be069dc1ada4b5e3a72010
Jemyung <leej@chikyu.ac.jp>
2021-08-31 10:14:16 +0900

    minor

--- Eurostat/EmissionAnalysis.jl
+++ Eurostat/EmissionAnalysis.jl
@@ -30,7 +30,7 @@ DE_mode = true              # direct carbon emission estimation
 DE_factor_estimate = true   # [true] estimate DE factors from IEA datasets, [false] read DE factors
 
 nation = "Eurostat"
-year = 2010
+year = 2015
 catDepth = 4
 depthTag = ["1st", "2nd", "3rd", "4th"]
 emission_unit = "tCO2"
