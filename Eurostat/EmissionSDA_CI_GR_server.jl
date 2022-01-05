ac850c96d5d0e2f80543a34c50e1b366f21b3290
JemyungLEE <leej@chikyu.ac.jp>
2022-01-05 16:25:30 +0900

    minor change

--- Eurostat/EmissionSDA_CI_GR_server.jl
+++ Eurostat/EmissionSDA_CI_GR_server.jl
@@ -233,7 +233,7 @@ for n in nats
                             resample_size = 0, replacement = true, visible = true, reuse = reuse_mem,
                             pop_dens = pop_dens, cf_intv = cf_gr, inc_intv = inc_gr, hpos_cf = pos_cf, hpos_inc = pos_inc,
                             cf_bndr = cf_bnd, inc_bndr = inc_bnd,
-                            iter = 10, min_itr = 5, chk_itr = 1, err_crt = 0.0001)
+                            iter = 2000, min_itr = 1000, chk_itr = 10, err_crt = 0.001)
     print(", printing")
     ed.printSdaCI_values(target_year, base_year, ci_file, n, ci_rate = 0.95, mode = sda_mode)
 
