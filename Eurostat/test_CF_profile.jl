d4e783269bacfd59c3ad1d9dce918bbb89666203
JemyungLEE <leej@chikyu.ac.jp>
2022-06-21 12:58:19 +0900

    CF profile: minor update

--- Eurostat/test_CF_profile.jl
+++ Eurostat/test_CF_profile.jl
@@ -15,8 +15,10 @@ year = 2015
 years = [2015]
 nutsLv = 1
 onlyNutsInHbs = true
-Qtable = "I_CHG_CO2"
+removeNTZ = true
+adjustNTZ = removeNTZ ? false : true
 
+Qtable = "_I_CHG_CO2"
 # Qtable = "_PRIMAP"
 
 ceIntegrateMode = "cf"      # "ie" (only indirect CE), "de" (only direct CE), or "cf" (integrage direct and indirect CEs)
@@ -25,6 +27,7 @@ catDepth = 4
 depthTag = ["1st", "2nd", "3rd", "4th"]
 
 cpi_scaling = true; base_year = 2010
+ConstConv = true    # convert household income and expenditure to constant year (=base_year) price
 
 substMode = true; if substMode; substTag = "_subst" else substTag = "" end
 scaleMode = true; if scaleMode; scaleTag = "Scaled_" else scaleTag = "" end
@@ -32,10 +35,8 @@ scaleMode = true; if scaleMode; scaleTag = "Scaled_" else scaleTag = "" end
 eqvalMode = false   # [true]: apply square root of household size for equivalance scale
 ntWeighMode = true  # [true]: apply NUTS population based weight, [false]:apply HBS weight
 
-
 popweight = true
 grid_pop = true
-expenditureMode = false
 
 filePath = Base.source_dir() * "/data/"
 indexPath = filePath * "index/"
@@ -43,8 +44,9 @@ extrPath = filePath * "extracted/"
 emissPath = filePath * "emission/" * string(year) * "/"
 indexFile = indexPath * "Eurostat_Index_ver5.0.xlsx"
 hhsfile = extrPath * string(year) * "_Households.csv"
-
 ExpenditureFile = extrPath * scaleTag * "Expenditure_matrix_4th" * substTag * ".csv"
+
+if ConstConv; hhsfile = replace(hhsfile, ".csv" => "_" * string(base_year) * "_constant.csv") end
 
 incomePeriod = "annual"  # Period: "annual"(default), "monthly", or "daily"
 
@@ -56,28 +58,26 @@ subcat=""
 print(" Data reading: ")
 print("category"); ec.readCategoryData(indexFile, year, nutsLv, except=["None"], subCategory=subcat)
 if length(subcat)==0; ec.setCategory(categories); else ec.setCategory(foodCategories); subcat *= "_" end
-print(", household"); ec.readHouseholdData(hhsfile, period = incomePeriod, remove = onlyNutsInHbs, alter=true)
+print(", household"); ec.readHouseholdData(hhsfile, period = incomePeriod, remove_nt=onlyNutsInHbs, remove_z=removeNTZ, alter=true)
 print(", population"); ec.readPopulation(year, indexFile, nuts_lv = nutsLv)
 print(", gridded population"); ec.readPopGridded(year, indexFile, nuts_lv = [nutsLv], adjust = true)
 print(", emission")
-if !expenditureMode
-    IE_files = []; DE_files = []; ie_nations = []; de_nations = []
-    ie_file_tag = "_hhs_"*scaleTag*"IE"*Qtable*".txt"
-    if cpi_scaling && base_year != year; ie_file_tag = replace(ie_file_tag, ".txt" => "_converted_" * string(base_year) * ".txt") end
-    de_file_tag = "_hhs_"*scaleTag*"DE.txt"
-    for f in readdir(emissPath)
-        if startswith(f, string(year)) && endswith(f, ie_file_tag); push!(IE_files, emissPath*f); push!(ie_nations, f[6:7])
-        elseif startswith(f, string(year)) && endswith(f, de_file_tag); push!(DE_files, emissPath*f); push!(de_nations, f[6:7])
-        end
+IE_files = []; DE_files = []; ie_nations = []; de_nations = []
+ie_file_tag = "_hhs_"*scaleTag*"IE"*Qtable*".txt"
+if cpi_scaling && base_year != year; ie_file_tag = replace(ie_file_tag, ".txt" => "_converted_" * string(base_year) * ".txt") end
+de_file_tag = "_hhs_"*scaleTag*"DE.txt"
+for f in readdir(emissPath)
+    if startswith(f, string(year)) && endswith(f, ie_file_tag); push!(IE_files, emissPath*f); push!(ie_nations, f[6:7])
+    elseif startswith(f, string(year)) && endswith(f, de_file_tag); push!(DE_files, emissPath*f); push!(de_nations, f[6:7])
     end
-    print("_IE"); ec.readEmissionData(year, ie_nations, IE_files, mode = "ie")
-    print("_DE"); ec.readEmissionData(year, de_nations, DE_files, mode = "de")
-    print("_CF"); ec.integrateCarbonFootprint(year, mode=ceIntegrateMode)
 end
+print("_IE"); ec.readEmissionData(year, ie_nations, IE_files, mode = "ie")
+print("_DE"); ec.readEmissionData(year, de_nations, DE_files, mode = "de")
+print("_CF"); ec.integrateCarbonFootprint(year, mode=ceIntegrateMode)
 println(" ... complete")
 
 print(" Weights calculating: ")
-ec.calculateNutsPopulationWeight(year = year, pop_dens = grid_pop, adjust = true)
+ec.calculateNutsPopulationWeight(year = year, pop_dens = grid_pop, adjust = adjustNTZ)
 println(" ... complete")
 
 print(" Profiling:")
