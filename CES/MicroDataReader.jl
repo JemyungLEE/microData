9de50dfb46bff41f7542a9ea896f8d2897aedf05
JemyungLEE <leej@chikyu.ac.jp>
2023-08-24 16:09:14 +0900

    expenditure matrix empty value part revised

--- CES/MicroDataReader.jl
+++ CES/MicroDataReader.jl
@@ -4,7 +4,7 @@
 module MicroDataReader
 
 # Developed date: 17. Mar. 2021
-# Last modified date: 21. Aug. 2023
+# Last modified date: 24. Aug. 2023
 # Subject: Household consumption expenditure survey microdata reader
 # Description: read consumption survey microdata and store household, member, and expenditure data
 # Developer: Jemyung Lee
@@ -1806,7 +1806,8 @@ function readExtractedExpenditureMatrix(year, nation, inputFile; quantity = fals
             hi = findfirst(x->x==s[1], hl)
             if hi !=nothing
                 push!(hhs, s[1])
-                mat[hi,:] = parse.(Float64, s[2:1+ns])[si]
+                mat[hi,:] = [ev == nothing ? 0.0 : ev for ev in [tryparse(Float64, s[ci]) for ci = 2:1+ns]][si]
+                # mat[hi,:] = parse.(Float64, s[2:1+ns])[si]
             end
         end
         close(f)
@@ -1864,8 +1865,8 @@ function filterGroupExpenditure(year, nation)
     ghidx = [filter(x -> hhs[hl[x]].group == g, 1:nh) for g in gl]
     ngsidx = [filter(x -> sc[sl[x]].group != g, 1:ns) for g in gl]
 
-    if haskey(expMatrix, y) && haskey(expMatrix[y], n); expMatrix[y][n][ghidx[gi], ngsidx[gi]] .= 0 end
-    if haskey(qntMatrix, y) && haskey(qntMatrix[y], n); qntMatrix[y][n][ghidx[gi], ngsidx[gi]] .= 0 end
+    if haskey(expMatrix, y) && haskey(expMatrix[y], n); for gi = 1:ng; expMatrix[y][n][ghidx[gi], ngsidx[gi]] .= 0 end end
+    if haskey(qntMatrix, y) && haskey(qntMatrix[y], n); for gi = 1:ng; qntMatrix[y][n][ghidx[gi], ngsidx[gi]] .= 0 end end
 end
 
 end
